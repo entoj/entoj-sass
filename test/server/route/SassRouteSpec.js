7f534c9b1620bb009b85116288474c0f4e6613a2
Christian Auth <c.auth@syzygy.de>
2017-06-14 16:34:05 +0200

    test(sass.server.route.SassRoute): fixes dependencies

--- test/server/route/SassRouteSpec.js
+++ test/server/route/SassRouteSpec.js
@@ -3,8 +3,9 @@
 /**
  * Requirements
  */
-const SassRoute = require(SASS_SOURCE + '/server/route/StaticFileRoute.js').StaticFileRoute;
+const SassRoute = require(SASS_SOURCE + '/server/route/SassRoute.js').SassRoute;
 const CliLogger = require('entoj-system').cli.CliLogger;
+const PathesConfiguration = require('entoj-system').model.configuration.PathesConfiguration;
 const routeSpec = require('entoj-system/test').server.RouteShared;
 
 
@@ -16,9 +17,10 @@ describe(SassRoute.className, function()
     /**
      * Route Test
      */
-    routeSpec(SassRoute, 'server.route/SassRoute', function(parameters)
+    routeSpec(SassRoute, 'sass.server.route/SassRoute', function(parameters)
     {
+        const pathesConfiguration = new PathesConfiguration();
         const cliLogger = new CliLogger('', { muted: true });
-        return [cliLogger];
+        return [cliLogger, pathesConfiguration];
     });
 });
